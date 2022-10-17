43d75b624b4a868769b896c9cdf70412c592e821
Novikov Aleksei <3900676@gmail.com>
2022-10-17 20:49:31 +0300

    add post test

--- src/test/java/in/reqres/ReqresInTests.java
+++ src/test/java/in/reqres/ReqresInTests.java
@@ -1,6 +1,10 @@
 package in.reqres;
 
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.hasItems;
@@ -9,14 +13,14 @@ import static org.hamcrest.core.IsEqual.equalTo;
 public class ReqresInTests {
 
     @Test
-    public void getUserSingleTest() {
-        String uriLink = "https://reqres.in/api/users/2";
+    void getUserSingleTest() {
+        RestAssured.baseURI = "https://reqres.in";
 
         given()
                 .log().uri()
                 .log().method()
                 .when()
-                .get(uriLink)
+                .get("/api/users/2")
                 .then()
                 .log().status()
                 .log().body()
@@ -26,14 +30,14 @@ public class ReqresInTests {
     }
 
     @Test
-    public void getListUserTest() {
-        String uriLink = "https://reqres.in/api/users?page=2";
+    void getListUserTest() {
+        RestAssured.baseURI = "https://reqres.in";
 
         given()
                 .log().uri()
                 .log().method()
                 .when()
-                .get(uriLink)
+                .get("/api/users?page=2")
                 .then()
                 .log().status()
                 .log().body()
@@ -42,5 +46,35 @@ public class ReqresInTests {
                 .body("data.id", hasItems(7, 8, 9));
     }
 
+    @Test
+    void postCreateUserTest() {
+        RestAssured.baseURI = "https://reqres.in";
 
+        Map<String, Object> jsonAsMap = new HashMap<>();
+        jsonAsMap.put("name", "morpheus");
+        jsonAsMap.put("job", "leader");
+
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType("application/json").
+                body(jsonAsMap).
+                when()
+                .post("/api/users")
+                .then()
+                .statusCode(201)
+                .log().status()
+                .log().body();
+    }
+
+    @Test
+    void postLoginUserTest() {
+        RestAssured.baseURI = "https://reqres.in";
+
+//        given()
+//                .auth().preemptive().basic("eve.holt@reqres.in", "cityslicka")
+//                .when().post("/api/login")
+//                .then().statusCode(200);
+    }
 }
