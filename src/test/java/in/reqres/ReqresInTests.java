33b8d4a07a9db200ff05ddafba153f167c2d7fae
Novikov Aleksei <3900676@gmail.com>
2022-10-17 18:44:40 +0300

    add 2*get api test

--- src/test/java/in/reqres/ReqresInTests.java
+++ src/test/java/in/reqres/ReqresInTests.java
@@ -3,6 +3,7 @@ package in.reqres;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.core.IsEqual.equalTo;
 
 public class ReqresInTests {
@@ -24,7 +25,22 @@ public class ReqresInTests {
                 .body("data.email", equalTo("janet.weaver@reqres.in"));
     }
 
+    @Test
+    public void getListUserTest() {
+        String uriLink = "https://reqres.in/api/users?page=2";
 
+        given()
+                .log().uri()
+                .log().method()
+                .when()
+                .get(uriLink)
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .assertThat()
+                .body("data.id", hasItems(7, 8, 9));
+    }
 
 
 }
